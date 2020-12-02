e63c7a8e8b3c3b1f2e18366b4db7d610c1263fcb
Császár Ákos <57495891+RockieHUN@users.noreply.github.com>
2020-12-02 09:16:47 +0200

    New main menu design, Favorites page

---
+++ app/src/main/java/com/example/luigi/fragments/FavoritesFragment.kt
@@ -0,0 +1,68 @@
+package com.example.luigi.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.addCallback
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.luigi.R
+import com.example.luigi.adapters.MainDataAdapter
+import com.example.luigi.databinding.FragmentFavoritesBinding
+import com.example.luigi.viewModels.MyDatabaseViewModel
+import java.util.*
+import kotlin.concurrent.timerTask
+
+
+class FavoritesFragment : Fragment(), MainDataAdapter.OnItemClickListener {
+
+    private lateinit var myDatabaseViewModel : MyDatabaseViewModel
+    private lateinit var binding : FragmentFavoritesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_favorites,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //onBackPressed navigate to the main fragment
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigate(R.id.action_favoritesFragment_to_mainMenuFragment)
+        }
+
+        //DATABASE VIEWMODEL
+        myDatabaseViewModel = requireActivity().run {
+            ViewModelProvider(requireActivity()).get(MyDatabaseViewModel::class.java)
+        }
+
+        //recycle view
+        myDatabaseViewModel.restaurants.observe(requireActivity(), { restaurants ->
+            binding.recycleView.adapter = MainDataAdapter(mutableListOf(), this)
+
+        })
+        binding.recycleView.layoutManager = LinearLayoutManager(context)
+        binding.recycleView.setHasFixedSize(true)
+    }
+
+    override fun onItemClick(position: Int, toString: String) {
+        myDatabaseViewModel.position = position
+        findNavController().navigate(R.id.action_favoritesFragment_to_detailFragment)
+    }
+
+
+}
